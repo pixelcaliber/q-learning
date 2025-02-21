fedb207eb34e5440b90e14af7796ef7168e2923b
Abhinav Pandey <79640544+pixelcaliber@users.noreply.github.com>
2025-02-21 21:29:41 +0000

    update model name

--- app/config.py
+++ app/config.py
@@ -8,6 +8,6 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-key-change-this")
     MODEL_SAVE_PATH = os.getenv(
-        "MODEL_SAVE_PATH", "instance/models/tictactoe_model_3.pkl"
+        "MODEL_SAVE_PATH", "instance/models/tictactoe_model.pkl"
     )
     RATE_LIMIT_STORAGE_URL = os.getenv("RATE_LIMIT_STORAGE_URL", "memory://")
